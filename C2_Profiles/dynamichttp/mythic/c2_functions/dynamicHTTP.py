69c3598b47456d2cfcc0d2b57f6501f450cd0c1d
Cody Thomas <codybthomas@gmail.com>
2021-03-21 21:20:22 -0700

    initial push for mythic 2.2

---
+++ C2_Profiles/dynamichttp/mythic/c2_functions/dynamicHTTP.py
@@ -0,0 +1,25 @@
+from mythic_c2_container.C2ProfileBase import *
+
+
+class DynamicHTTP(C2Profile):
+    name = "dynamichttp"
+    description = "Manipulate HTTP(S) requests and responses"
+    author = "@its_a_feature_"
+    is_p2p = False
+    is_server_routed = False
+    mythic_encrypts = True
+    translation_container = None
+    parameters = [
+        C2ProfileParameter(
+            name="AESPSK",
+            description="Crypto type",
+            default_value="aes256_hmac",
+            parameter_type=ParameterType.ChooseOne,
+            choices=["aes256_hmac", "none"],
+            required=False,
+            crypto_type=True
+        ),
+        C2ProfileParameter(
+            name="raw_c2_config", description="Agent JSON Config", default_value=""
+        ),
+    ]
